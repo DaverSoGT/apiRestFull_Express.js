e82f621ca314001c766136c0bd6d58bc1cfde84a
David Herrera <jodaniheso@gmail.com>
2021-10-19 22:12:21 -0600

    add library faker to project and add more get methods

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const faker = require('faker')
 const app = express()
 const port = 3000
 
@@ -11,11 +12,19 @@ app.get('/nueva-ruta', (req, res) => {
 })
 
 app.get('/products', (req, res) => {
-  res.json({
-    name: 'Product 1',
-    price: 1000
-  })
+  const products = []
+  const { size } = req.query
+  const limit = size || 10
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl()
+    })
+  }
+  res.json(products)
 })
+
 
 app.get('/categories', (req, res) => {
   res.json([
@@ -29,6 +38,11 @@ app.get('/categories', (req, res) => {
     }
   ])
 })
+
+app.get('/categories/filter', (req, res) => {
+  res.send('soy un filter')
+})
+
 
 app.get('/categories/:id', (req, res) => {
   const { id } = req.params
@@ -67,6 +81,18 @@ app.get('/people/:peopleId', (req, res) => {
   res.json({
     peopleId
   })
+})
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    })
+  } else {
+    res.send('No hay parametros')
+  }
 })
 
 app.listen(port, () => {
