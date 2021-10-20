ac16b82c7aa8985f5a6eb2a3ab9e7b414bcdf6a2
David Herrera <jodaniheso@gmail.com>
2021-10-19 21:50:23 -0600

    add more get methods with id

--- index.js
+++ index.js
@@ -30,6 +30,45 @@ app.get('/categories', (req, res) => {
   ])
 })
 
+app.get('/categories/:id', (req, res) => {
+  const { id } = req.params
+  res.json({
+    id,
+    name: 'Tech',
+    apartment: 'PC'
+  })
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId} = req.params
+  res.json({
+    categoryId,
+    productId
+  })
+})
+
+app.get('/people', (req, res) => {
+  res.json([
+    {
+      name: 'Joaquin',
+      age: 30,
+      email: 'joaquin@user.com'
+    },
+    {
+      name: 'Jones',
+      age: 26,
+      email: 'jones@user.com'
+    }
+  ])
+})
+
+app.get('/people/:peopleId', (req, res) => {
+  const { peopleId } = req.params
+  res.json({
+    peopleId
+  })
+})
+
 app.listen(port, () => {
   console.log('Mi port ' + port)
 })
