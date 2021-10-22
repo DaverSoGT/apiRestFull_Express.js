d066d80090ea28e7ff3df832a2d6a56bf0c45fcf
David Herrera <jodaniheso@gmail.com>
2021-10-22 16:13:01 -0600

    added cors, .html file, valitation with joi

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const routerApi = require('./routes')
+const cors = require('cors')
+
 const app = express()
 const port = 3000
 
 const { logErrors, errorHandler } = require('./middlewares/error.handler')
 
 app.use(express.json())
+
+const whitelist = ['http://localhost:5500', 'https://myapp.co']
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('No he permitido'))
+    }
+  }
+}
+app.use(cors(options))
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express')
